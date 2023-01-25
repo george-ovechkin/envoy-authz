fb48cfc32b48eaa6c9a9436ffc71f922107bc59f
george.ovechin <no@mail>
2023-01-25 19:30:25 +0400

    full signout feature

--- auth/src/main/java/org/example/authserver/config/Constants.java
+++ auth/src/main/java/org/example/authserver/config/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
   public static final String ACL_REDIS_KEY = "acls";
   public static final String ACL_REL_CONFIG_REDIS_KEY = "rel_configs";
   public static final String SIGNOUT_REDIS_KEY = "SIGNOUT_%s";
+  public static final String FULL_SIGNOUT_REDIS_KEY = "FULL_SIGNOUT_%s";
 }
